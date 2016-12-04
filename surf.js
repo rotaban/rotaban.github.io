035f68a585945776d4b19942be50b5d5eb834305
rotaban <AlexNirt12@ex.ua>
2016-12-04 17:51:25 +0200

    Add files via upload

---
+++ surf.js
@@ -0,0 +1,122 @@
+var s=[
+"ferst.html",
+"http://rollingfaucet.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://freebitcoinstoyou.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://bitcoinnewsday.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"perehod3.htm",
+"http://www.claimbit.co.uk/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://www.breaktheco.in/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://faucet.jokertimes.co/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://faucetplanet.net/bitcoin.php?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://safebitco.in/index.php?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://bitcoinfree.es/faucet.php?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"perehod2.htm",
+"http://playbitco.in/?ref=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"perehod.htm",
+"http://bazarofbitoins.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://bitcobear.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://komafaucet.ru/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://bitkranik.ru/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://andfaucet.ru/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://satoshik.ru/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://bitcoin.cryptorials.io/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://your-bit.co.in/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://welovebtc.com/?ref=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://super-bitcoins.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://www.luckybitfaucet.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://cryptofree.info/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://infaucet.com/?id=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://24bitcoin.ru/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://oneadfaucet.com/btc/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://thefaucetfarmer.com/?id=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"perehod5.htm",
+"http://qoinfaucet.com/?id=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://coinator.net/btc/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://www.investmentwp.com/faucet/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://btc.freecoin.space/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://thefaucetbanker.com/?id=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://www.starsfaucet.com/faucet.php?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://electrum-faucet.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://rapidbitco.in/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://wallabycoin.link/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://speedbitcoin.net/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://superdeal-faucet.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://faucet.happyputin.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://faucet.pinktussy.co/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://moneyinpjs.com/fast-bitcoin/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://www.faucetsfree.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"perehod4.htm",
+"http://www.letsgoo.de/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://faucetworld.eu/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://freebitcoinhome.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://www.faucetbitcoins.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://yoshifaucet.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://boxclikz.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://freebtcforall.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://neobitcoinfaucet.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://btcoinsworld.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://www.tracecoinfaucet.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://kofaucet.ru/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://bitcoinmarketbtc.com/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://www.freebitcoinsgolds.info/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+"http://partybang.eu/?r=1Fm7TNCPsyYwgrRdYVqBC12KRaHpVX4N2y",
+];
+
+////////////////////////////////////////////////////
+
+	var lis = document.getElementById("faucets").getElementsByTagName("li");
+	var current = document.getElementById("current");
+
+	var defaultValue = 0;
+	var minValue = 0;
+	var maxValue = s.length;
+
+	var counter = Math.max(Math.min(defaultValue, maxValue), minValue);
+
+	function previous() {
+		counter -= 1;
+		counter = Math.max(Math.min(counter, maxValue), minValue);
+		update();
+	}
+	
+	function next() {
+		counter += 1;
+		counter = Math.max(Math.min(counter, maxValue), minValue);
+		update();
+	}
+	
+	function jump(x) {
+		document.getElementById("myframe").src=s[x]
+		counter = x;
+		
+		// Set the current label
+		current.innerHTML = "Faucet " + x + " of " + (s.length -1);
+	}
+	
+	function update() {
+		document.getElementById("myframe").src=s[counter];
+		
+		// Highlight li links using the current counter value
+		$('#faucets').children().removeClass('active');
+		lis[counter].className="active";
+		
+		// Set the current label
+		current.innerHTML = "Faucet " + counter + " of " + (s.length -1);
+	}
+	
+	update();
+	
+////////////////////////////////////////////////////
+
+	// Highlight the last li link clicked
+	$(".nav-sidebar").on('click', 'li', function(e) {
+		$(this).parent().find('li.active').removeClass('active');
+		$(this).addClass('active');
+	});
+	
+	// Display Boopstrap based popup tooltips
+	$(document).ready(function(){
+		$('[data-toggle="tooltip"]').tooltip({
+			placement : 'bottom'
+		});
+	});
